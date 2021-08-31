4d20b960475a78c3b0382ee1f071deb3615c798e
LethsOliveira <lethsoliveira89@gmail.com>
2021-08-31 17:09:55 -0300

    criaçao da div de tempo

--- src/components/weather/index.js
+++ src/components/weather/index.js
@@ -1,8 +1,8 @@
 export function weather() {
   const widgetWeather = document.querySelector('');
   widgetWeather.innerHTML = '';
-
-}
+  const weatherWidget = document.querySelector('');
+  weatherWidget.innerHTML = `
 <iframe src="https://www.meteoblue.com/en/weather/widget/three?geoloc=detect&nocurrent=0&noforecast=0&days=4&tempunit=CELSIUS&windunit=KILOMETER_PER_HOUR&layout=image"
   frameborder="0" scrolling="NO" allowtransparency="true" sandbox="allow-same-origin allow-scripts allow-popups allow-popups-to-escape-sandbox"
   style="width: auto; height: auto;"></iframe>
@@ -11,3 +11,5 @@ export function weather() {
 <a href="https://www.meteoblue.com/en/weather/week/index?utm_source=weather_widget&utm_medium=linkus&utm_content=three&utm_campaign=Weather%2BWidget"
  target="_blank">meteoblue</a>
 </nav>
+`;
+}
