aedb438570f4639ea2e1825425ea34695e6b2807
jenniferpessoa <jenni.pessoa@gmail.com>
2021-09-01 18:07:34 -0300

    Perfil do Feed, Validação do Texto, Modal Delete

--- src/components/popup/index.js
+++ src/components/popup/index.js
@@ -78,4 +78,59 @@ const popUpNotice = (template) => {
 //   return root;
 // }
 
+
+
+export const modal = {
+  confirm: (message, callback) => {
+    const template = `<div id="modal-confirm" class="modal-background">
+        <div class="modal">
+          <span id="close-btn">&times;</span>
+          <p class='modalMensage'>${(message) || '???'}</p>
+          <div class="buttons">
+            <button class="yes" id="confirm-btn"> Sim </button>
+            <button class="no" id="no-btn"> Não </button>
+          </div>
+        </div>
+      </div>`;
+
+    const modalElement = document.createElement('div');
+
+    modalElement.innerHTML = template;
+
+    document.body.appendChild(modalElement);
+
+    const confirmBtn = modalElement.querySelector('#confirm-btn');
+    //console.log(confirmBtn)
+    const modalBackground = modalElement.querySelector('.modal-background');
+    //console.log(modalBackground)
+    const closeBtn = modalElement.querySelector('#close-btn');
+    //console.log(closeBtn)
+    const noBtn = modalElement.querySelector('#no-btn');
+    //console.log(noBtn)
+
+    confirmBtn.addEventListener('click', () => {
+      callback();
+      modalBackground.style.display = 'none';
+    });
+
+    closeBtn.addEventListener('click', () => {
+      modalBackground.style.display = 'none';
+    });
+
+    noBtn.addEventListener('click', () => {
+      modalBackground.style.display = 'none';
+    });
+
+    window.addEventListener('click', (event) => {
+      if (event.target === modalBackground) {
+        modalBackground.style.display = 'none';
+      }
+    });
+
+    
+  },
+
+};
+
+
 export { popUpNotice };
